1bef1f7d74161792242548ac29b7c6a31fd39f61
Anselm von Wangenheim <anselm.w@gmx.de>
2016-06-24 20:26:39 +0200

    csv spec reader..

---
+++ csv.spec
@@ -0,0 +1,1 @@
+MAC,IP,PING,PARENT\n
--- src/de/sfn_kassel/NetBackend.java
+++ src/de/sfn_kassel/NetBackend.java
@@ -1,14 +1,34 @@
 package de.sfn_kassel;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by anselm on 24.06.16.
  */
-public class NetBackend {
+public class NetBackend implements  Runnable {
+
+    private final Scanner robIN;
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
 
-    public NetBackend() {
+    public NetBackend(File cPath) throws IOException {
+        Process cProcess = Runtime.getRuntime().exec(cPath.getAbsolutePath());
+        robIN = new Scanner(cProcess.getInputStream());
+    }
+
+    @Override
+    public void run() {
+        String line;
+        while ((line = robIN.nextLine()) != null) {
+            parseLine(line);
+        }
+    }
+
+    private void parseLine(String line) {
 
     }
 }
