5c8db19267095501d447bcd810148bdf9b961aba
Anselm von Wangenheim <anselm.w@gmx.de>
2016-06-24 20:13:47 +0200

    hello

---
+++ src/de/sfn_kassel/NetBackend.java
@@ -0,0 +1,14 @@
+package de.sfn_kassel;
+
+/**
+ * Created by anselm on 24.06.16.
+ */
+public class NetBackend {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    public NetBackend() {
+
+    }
+}
